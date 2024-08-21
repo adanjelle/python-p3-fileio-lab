ace3e0504393824e2c821ce7e797d7089cf3e397
adanjelle <adanjelle4@gmail.com>
2024-08-21 22:50:20 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
+
 def write_file(file_name, file_content):
-    pass
+    # Ensure the file name has a .txt extension
+    file_name = f"{file_name}.txt"
+    # Open the file in write mode, which will overwrite any existing content
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Ensure the file name has a .txt extension
+    file_name = f"{file_name}.txt"
+    # Open the file in append mode, which will add content to the end of the file
+    with open(file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    # Ensure the file name has a .txt extension
+    file_name = f"{file_name}.txt"
+    # Open the file in read mode and return its content
+    with open(file_name, 'r') as file:
+        return file.read()
